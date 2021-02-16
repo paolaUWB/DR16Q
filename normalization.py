166519cad0b12a8f13aba249e4a168f2d33488c9
paolaUWB <73371145+paolaUWB@users.noreply.github.com>
2021-02-15 19:37:46 -0800

    Update normalization.py

--- normalization.py
+++ normalization.py
@@ -16,7 +16,7 @@ from scipy.optimize import curve_fit
 from matplotlib.backends.backend_pdf import PdfPages
 from utility_functions import print_to_file, clear_file, append_row_to_csv
 from data_types import Range, ColumnIndexes, PointData, RangesData, FigureData, FigureDataOriginal, DataNormalized
-from wavelength_flux_error_for_points import wavelength_flux_error_for_points
+from useful_wavelength_flux_error_modules import wavelength_flux_error_for_points, wavelength_flux_error_in_range
 
 # Space for variables, X that might need to changed -------------------------------------------------
 
@@ -31,7 +31,7 @@ column_index = ColumnIndexes(0, 1, 2)
 # Sets the directory to find the data files (dr9, dr16)
 SPEC_DIREC = os.getcwd() + "/DATA/" # Set location of input and output spectrum files XXX Set a different one for input & output US LATER
 
-STARTS_FROM, ENDS_AT = 1, 9 # Range of spectra you are working with from the quasar names file. XXX be able to write from 1 to 10 and end in 10. US LATER
+STARTS_FROM, ENDS_AT = 1, 10 # Range of spectra you are working with from the quasar names file. XXX be able to write from 1 to 10 and end in 10. US LATER
 
 # Ranges of wavelengths in the spectra for different tasks
 WAVELENGTH_RESTFRAME = Range(1200., 1800.)
@@ -65,57 +65,26 @@ c = -0.5 # initial parameter of powerlaw
 def powerlaw(wavelength, b, c) -> float:
     return b * (np.power(wavelength, c))
 
-
 def define_three_anchor_points(z: float, spectra_data):
-    left_point_ranges = wavelength_flux_error_for_points(
+    left_point = wavelength_flux_error_for_points(
         WAVELENGTH_RESTFRAME_FOR_LEFT_POINT.start,
         WAVELENGTH_RESTFRAME_FOR_LEFT_POINT.end,
         z,
         spectra_data)
-    left_point = PointData(
-        np.median(left_point_ranges.wavelength),
-        np.median(left_point_ranges.flux),
-        np.median(left_point_ranges.error))
 
-    print(left_point_ranges.flux)
-    print_to_file(left_point_ranges.flux, LOG_FILE)
-
-    middle_point_ranges = wavelength_flux_error_for_points(
+    middle_point = wavelength_flux_error_for_points(
         WAVELENGTH_RESTFRAME_FOR_MIDDLE_POINT.start,
         WAVELENGTH_RESTFRAME_FOR_MIDDLE_POINT.end,
         z,
         spectra_data)
-    middle_point = PointData(
-        np.median(middle_point_ranges.wavelength),
-        np.median(middle_point_ranges.flux),
-        np.median(middle_point_ranges.error))
-
-    right_point_ranges = wavelength_flux_error_for_points(
+   
+    right_point = wavelength_flux_error_for_points(
         WAVELENGTH_RESTFRAME_FOR_RIGHT_POINT.start,
         WAVELENGTH_RESTFRAME_FOR_RIGHT_POINT.end,
         z,
         spectra_data)
-    right_point = PointData(
-        np.median(right_point_ranges.wavelength),
-        np.median(right_point_ranges.flux),
-        np.median(right_point_ranges.error))
-
+    
     return (left_point, middle_point, right_point)
-
-def wavelength_flux_error_in_range(starting_point: float, ending_point: float, z: float, spectra_data) -> RangesData:
-    wavelength_column = spectra_data[:, column_index.wavelength]
-
-    wavelength_observed_from = (z + 1) * starting_point
-    wavelength_observed_to = (z + 1) * ending_point
-
-    wavelength_lower_limit = np.where(wavelength_column > wavelength_observed_from)
-    wavelength_upper_limit = np.where(wavelength_column < wavelength_observed_to)
-    
-    wavelength = spectra_data[np.min(wavelength_lower_limit[column_index.wavelength]):np.max(wavelength_upper_limit[column_index.wavelength]), column_index.wavelength]
-    flux = spectra_data[np.min(wavelength_lower_limit[column_index.wavelength]): np.max(wavelength_upper_limit[column_index.wavelength]), column_index.flux]
-    error = spectra_data[np.min(wavelength_lower_limit[column_index.wavelength]): np.max(wavelength_upper_limit[column_index.wavelength]), column_index.error]
-    
-    return RangesData(wavelength, flux, error)
 
 def draw_original_figure(figure_index: int, original_ranges: RangesData, data: FigureDataOriginal, test1: RangesData, test2: RangesData):
     main_color = "xkcd:ultramarine"
@@ -135,17 +104,17 @@ def draw_original_figure(figure_index: int, original_ranges: RangesData, data: F
     ORIGINAL_PDF.savefig()
     plt.close(figure_index)
 
-def draw_normalized_figure(figure_index: int, original_ranges: RangesData, figure_data: FigureData, normalized_data: DataNormalized,
+def draw_normalized_figure(figure_index: int, original_ranges: RangesData, figure_data: FigureData, flux_normalized, error_normalized, #1 param more since I removed the tuple
                             test1: RangesData, test2: RangesData, normalized_flux_test_1, normalized_flux_test_2):
     main_color = "xkcd:ultramarine"
     test_1_color, test_2_color = "xkcd:green apple", "xkcd:bubblegum"
     subtitle_text = f"z={figure_data.z} snr={figure_data.snr} snr_mean_in_ehvo={figure_data.snr_mean_in_ehvo}"
     plt.figure(figure_index) 
-    plt.text(((figure_data.wavelength_from + figure_data.wavelength_to)/2.3), np.max(normalized_data.flux_normalized)/1.07, figure_data.spectrum_file_name)
-    plt.text(((figure_data.wavelength_from + figure_data.wavelength_to)/2.3), np.max(normalized_data.flux_normalized), subtitle_text)
+    plt.text(((figure_data.wavelength_from + figure_data.wavelength_to)/2.3), np.max(flux_normalized)/1.07, figure_data.spectrum_file_name)
+    plt.text(((figure_data.wavelength_from + figure_data.wavelength_to)/2.3), np.max(flux_normalized), subtitle_text)
     plt.title(figure_data.spectrum_file_name)
-    plt.plot(original_ranges.wavelength, normalized_data.flux_normalized, color = main_color, linestyle = "-")
-    plt.plot(original_ranges.wavelength, normalized_data.error_normalized, color = "black", linestyle = "-")
+    plt.plot(original_ranges.wavelength, flux_normalized, color = main_color, linestyle = "-")
+    plt.plot(original_ranges.wavelength, error_normalized, color = "black", linestyle = "-")
     plt.title("Normalized Data vs. Normalized Error")
     plt.xlabel("Wavelength [A]")
     plt.ylabel("Normalized Flux[10^[-17]]cgs")
@@ -155,12 +124,32 @@ def draw_normalized_figure(figure_index: int, original_ranges: RangesData, figur
     NORMALIZED_PDF.savefig()
     plt.close(figure_index)
 
-def process_spectra_and_draw_figures(index: int, z, snr, spectrum_file_name):
+spectra_list, redshift_value_list, snr_value_list = [], [], []
 
-    print(str(index) + ": " + spectrum_file_name)
-    print_to_file(str(index) + ": " + spectrum_file_name, LOG_FILE)
+# Reading the file and assigning to the specific lists
+with open(CONFIG_FILE) as f:   # XXX FOR MMC WFGN What exactly does the "with" do
+    for line in f:
+        each_row_in_file = line.split(",")
+        spectra_list.append(each_row_in_file[0])
+        redshift_value_list.append(np.float(each_row_in_file[1]))
+        snr_value_list.append(np.float(each_row_in_file[2]))
 
-    current_spectra_data = np.loadtxt(SPEC_DIREC + spectrum_file_name)
+indices, spectra_indices, processed_spectra_file_names, powerlaw_final_b_values, powerlaw_final_c_values = [], [], [], [], []
+flagged_indices, flagged_spectra_indices, flagged_spectra_file_names = [], [], []
+flagged_snr_indices, flagged_snr_spectra_indices, flagged_snr_spectra_file_names, flagged_snr_in_ehvo_values = [], [], [], []
+
+for spectra_index in range(STARTS_FROM, ENDS_AT + 1):
+    z = round(redshift_value_list[spectra_index - 1], 5)
+    snr = round(snr_value_list[spectra_index - 1], 5)
+    current_spectrum_file_name = spectra_list[spectra_index - 1]
+    
+    ###spectra_index, z, snr, current_spectrum_file_name
+    ###index: int, z, snr, spectrum_file_name
+
+    print(str(spectra_index) + ": " + current_spectrum_file_name)
+    print_to_file(str(spectra_index) + ": " + current_spectrum_file_name, LOG_FILE)
+
+    current_spectra_data = np.loadtxt(SPEC_DIREC + current_spectrum_file_name)
 
     wavelength_observed_from = (z + 1) * WAVELENGTH_RESTFRAME.start
     wavelength_observed_to = (z + 1) * WAVELENGTH_RESTFRAME.end
@@ -191,8 +180,9 @@ def process_spectra_and_draw_figures(index: int, z, snr, spectrum_file_name):
 
     flux_normalized = flux/powerlaw(wavelength, bf, cf)
     error_normalized = error/powerlaw(wavelength, bf, cf)
-    normalized_data = DataNormalized(flux_normalized, error_normalized)
-
+    
+    ### VAMOS POR AQUI!
+    
     for n in range(1, len(flux_normalized) - 5):          
         if abs(flux_normalized[n + 1] - flux_normalized[n]) > 0.5:
             if error_normalized[n + 1] > 0.25:
@@ -248,7 +238,7 @@ def process_spectra_and_draw_figures(index: int, z, snr, spectrum_file_name):
         
     ##########################################################
 
-    ############# SNR Calculations ##########################
+    ############# SNR Calculations ##########################   ### MOVE THIS EARLIER
     wavelengths_for_snr_lower = np.where (wavelength/(z + 1.) < WAVELENGTH_FOR_SNR.start)
     wavelengths_for_snr_upper = np.where (wavelength/(z + 1.) > WAVELENGTH_FOR_SNR.end)
     snr_mean_in_ehvo = round(np.mean(1./error_normalized[np.max(wavelengths_for_snr_lower[0]):np.min(wavelengths_for_snr_upper)]), 5)
@@ -262,52 +252,35 @@ def process_spectra_and_draw_figures(index: int, z, snr, spectrum_file_name):
     original_figure_data = FigureDataOriginal(figure_data, bf, cf, power_law_data_x, power_law_data_y)
     
     draw_original_figure(index, original_ranges, original_figure_data, test1, test2)
-    draw_normalized_figure(index, original_ranges, figure_data, normalized_data, test1, test2, normalized_flux_test_1, normalized_flux_test_2)
+    draw_normalized_figure(index, original_ranges, figure_data, flux_normalized, error_normalized, test1, test2, normalized_flux_test_1, normalized_flux_test_2)
 
     norm_w_f_e = (wavelength, flux_normalized, error_normalized)
     norm_w_f_e = (np.transpose(norm_w_f_e))  
     np.savetxt(SPEC_DIREC + spectrum_file_name[0:20] + NORM_FILE_EXTENSION, norm_w_f_e)
-    return bf, cf, flagged, flagged_snr_mean_in_ehvo, snr_mean_in_ehvo
+    
+    return bf, cf, flagged, flagged_snr_mean_in_ehvo, snr_mean_in_ehvo         ## OLD END OF PROCESS... 
 
-main(STARTS_FROM, ENDS_AT)
-main(starting_index: int, ending_index: int):
-    spectra_list, redshift_value_list, snr_value_list = [], [], []
+ #XXX OLD END OF MAIN from here
+    
+ # add condition here?
+    powerlaw_final_b_values.append(b_final)
+    powerlaw_final_c_values.append(c_final)
+    processed_spectra_file_names.append(current_spectrum_file_name)
+    indices.append(spectra_index - starting_index + 1)
+    spectra_indices.append(spectra_index)
+    if failed_test:
+        flagged_spectra_file_names.append(current_spectrum_file_name)
+        flagged_indices.append(spectra_index - starting_index + 1)
+        flagged_spectra_indices.append(spectra_index)
 
-    # Reading the file and assigning to the specific lists
-    with open(CONFIG_FILE) as f:
-        for line in f:
-            each_row_in_file = line.split(",")
-            spectra_list.append(each_row_in_file[0])
-            redshift_value_list.append(np.float(each_row_in_file[1]))
-            snr_value_list.append(np.float(each_row_in_file[2]))
-
-    indices, spectra_indices, processed_spectra_file_names, powerlaw_final_b_values, powerlaw_final_c_values = [], [], [], [], []
-    flagged_indices, flagged_spectra_indices, flagged_spectra_file_names = [], [], []
-    flagged_snr_indices, flagged_snr_spectra_indices, flagged_snr_spectra_file_names, flagged_snr_in_ehvo_values = [], [], [], []
-
-    for spectra_index in range(starting_index, ending_index + 1):
-        z = round(redshift_value_list[spectra_index - 1], 5)
-        snr = round(snr_value_list[spectra_index - 1], 5)
-        current_spectrum_file_name = spectra_list[spectra_index - 1]
-        b_final, c_final, failed_test, flagged_snr_mean_in_ehvo, snr_mean_in_ehvo = process_spectra_and_draw_figures(spectra_index, z, snr, current_spectrum_file_name)
-       
-        # add condition here?
-        powerlaw_final_b_values.append(b_final)
-        powerlaw_final_c_values.append(c_final)
-        processed_spectra_file_names.append(current_spectrum_file_name)
-        indices.append(spectra_index - starting_index + 1)
-        spectra_indices.append(spectra_index)
-        if failed_test:
-            flagged_spectra_file_names.append(current_spectrum_file_name)
-            flagged_indices.append(spectra_index - starting_index + 1)
-            flagged_spectra_indices.append(spectra_index)
+    if flagged_snr_mean_in_ehvo:
+        flagged_snr_spectra_file_names.append(current_spectrum_file_name)
+        flagged_snr_indices.append(spectra_index - starting_index + 1)
+        flagged_snr_spectra_indices.append(spectra_index)
+        flagged_snr_in_ehvo_values.append(snr_mean_in_ehvo)
         
-        if flagged_snr_mean_in_ehvo:
-            flagged_snr_spectra_file_names.append(current_spectrum_file_name)
-            flagged_snr_indices.append(spectra_index - starting_index + 1)
-            flagged_snr_spectra_indices.append(spectra_index)
-            flagged_snr_in_ehvo_values.append(snr_mean_in_ehvo)
-
+     #XXX OLD END OF MAIN to here
+            
     final_initial_parameters = [indices, spectra_indices, processed_spectra_file_names, powerlaw_final_b_values, powerlaw_final_c_values]
     final_initial_parameters = (np.transpose(final_initial_parameters))
 
