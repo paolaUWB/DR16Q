1ff283572526ff9f22b455d64172eaff640b02bd
Nathnael Kahassai <nkahassai@unc.edu>
2021-07-28 18:20:34 -0500

    workflow49

--- abs_plot.py
+++ abs_plot.py
@@ -25,7 +25,7 @@ def draw_abs_figure(velocity, flux_normalized, error, savefile_name, spectra_nam
     plt.xlim(-70000, 0)
     max_peak = (np.mean(flux_normalized) * 1.9)
     min_peak = (np.min(error) - .5) 
-    plt.title((max_peak - .4), str(spectra_name) + ', z=' + str(redshift) + ' snr=' + str(snr))
+    plt.title(str(spectra_name) + ', z=' + str(redshift) + ' snr=' + str(snr))
     plt.axhline(y = 0.9, color='r', linestyle = '--')    
     plt.axhline(y = 1.0)
     plt.ylim((min_peak, max_peak))
