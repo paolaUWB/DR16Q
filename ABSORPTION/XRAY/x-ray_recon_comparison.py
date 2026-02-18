bec2c0749135a78ef2e8ab5f3015a11c48a1b1de
Elijah Facklam <boyzombie04@gmail.com>
2026-02-17 17:52:21 -0800

    Code For x-ray_recon_comparison update
    Made it so we can compare morphed and og and save as pdf
    
    Note: Still needs work

--- ABSORPTION/XRAY/x-ray_recon_comparison.py
+++ ABSORPTION/XRAY/x-ray_recon_comparison.py
@@ -7,10 +7,12 @@ Created on Fri Feb  6 15:25:15 2026
 """
 
 import os
+import matplotlib.pyplot as plt
 import sys
 import numpy as np
 from astropy.io import fits
 from matplotlib.backends.backend_pdf import PdfPages
+from spectra_comparison import Plot_spec_compare_morphed, Plot_spec_compare_og
 sys.path.insert(0, os.getcwd()+'/../')
 from abs_plot_module import draw_abs_figure
 from utility_functions import read_list_spectra
@@ -32,7 +34,7 @@ norm_spectra_list, redshift_list, calc_snr_list = read_list_spectra(CONFIG_FILE,
 
 
 vlast = []
-# whether abs_count or all_count is used is based on the value of all_plot_and_text
+# whether abs_count or all_count is used is based on the value of all_plot_and_text|
 abs_count = 0 # counter for amount of spectra that have absorption when all_plot_and_text = no and for text files
 all_count = 0 # counter for all spectra ran when all_plot_and_text = yes
 
@@ -40,58 +42,66 @@ all_count = 0 # counter for all spectra ran when all_plot_and_text = yes
 not_in_range = []
 
 # loops over each spectra from a specified starting and ending point
-for spectra_index in range(STARTS_FROM, ENDS_AT + 1): 
-    #keeping track of how many spectra do not have data points within the velocity limits
-    
-    # rounding the numbers of the redshift, calculated snr and setting the norm file name to the current file name from the csv
-    #z = round(redshift_list[spectra_index - 1], 5)
-    z = 0  #EDITED to be able to include correct z value in plot but not correct for redshift when plotting in velocity
-    z_final = round(redshift_list[spectra_index - 1], 2)
+output_morphed_pdf = "spectra_output_morphed.pdf"
+output_og_pdf = "spectra_output_og.pdf"
 
-    calc_snr = round(calc_snr_list[spectra_index - 1], 5)
-    norm_spectrum_file_name = norm_spectra_list[spectra_index - 1]
+with PdfPages(output_morphed_pdf) as pdf:
+    for spectra_index in range(STARTS_FROM, ENDS_AT + 1):
+
+        norm_spectrum_file_name = norm_spectra_list[spectra_index - 1]
+        File = os.getcwd() + "/Recons_Hiremath2025/" + str(norm_spectrum_file_name)
+
+        fig = Plot_spec_compare_morphed(File)
+
+        pdf.savefig(fig)
+        plt.show()
+        plt.close(fig)
+
+#for spectra_index in range(STARTS_FROM, ENDS_AT + 1): 
+#    #keeping track of how many spectra do not have data points within the velocity limits
+#    
+#    norm_spectrum_file_name = norm_spectra_list[spectra_index - 1]
+#    File = (os.getcwd() + "/Recons_Hiremath2025/" + str(norm_spectrum_file_name))
+#    fig = Plot_spec_compare_morphed(File)
+#    PdfPages.savefig(fig)
+#    fig.save()
+#    pyplot.gcf()
+#    plt.show()
+#    plt.close()
     
-    #EDITED: To accomodate xray spectra in .fits format....................................................
-    
-    # from the norm spectra name retrieving it's wavelength, normalized flux, and normalized error (in this case from NORM_DRXQ)
-   
-    # setting a variable for each of those values from the spectra
-    #wavelength, normalized_flux, normalized_error = read_spectra(norm_spectra_data) vvvvvv
-    #STOP Liliana come fix this before uploading to Github
-    File = fits.open(os.getcwd() + "/Recons_Hiremath2025/" + str(norm_spectrum_file_name))
+    '''
     data = File[1].data   
     
-    '''
+    
+    we want to compare, original, reconstruction and error in terms of velocity (check line 75)
+    look at spectra comparison program, examples of what the flux needs to be
+    
+    can feed the reconstruction and the original through the function draw_abs_figure; inside abs_plot_module
+    alternatively, call the function defined within the spectra_comparison that can make the plots displayed, fairly automated
+    
+    whichever one was chosen, have it produce into a pdf, we found a function in absorption.py; check line 13
     
     '''
-    wavelength = data['Wave']
-    flux = data["Flux"]
-    normalized_error = data["Noise"] #is this normalized as it is though? Does the error array need to be divided by recon?
-    recon = data["Recon"]
-    normalized_flux = flux/recon
-    #Compare reconstruction line and morphed line; morphed = flux / recon
-    
-    #EDITED: This section so that the velocity search limit would adjust if the spectra does not reach the full range
-    beta_test = wavelength_to_velocity(z, wavelength)
-    
-    
-    VELOCITY_LIMIT = Range(-30000, -60000)
-
-    min_beta = np.min(beta_test)
-    
-    if min_beta < VELOCITY_LIMIT.start:
-        # Compute the new end but clamp it at -60000
-        new_end = min(-min_beta, 60000)
-        VELOCITY_LIMIT = Range(-30000, -new_end)
-    else:
-        VELOCITY_LIMIT = VELOCITY_LIMIT
-        
-    draw_abs_figure(0, spectra_index)
-    
-    
     #draw_abs_figure(spectra_count_abs, spectra_index, velocity, flux_normalized, error, savefile_name, spectra_name, redshift, snr, max_peak):
+    
+    
+    '''
+    RECONSTRUCTION_MORPHED_OUTPUT_PLOT_PDF = PdfPages(OUT_DIREC + 'BI' + str(BALNICITY_INDEX_LIMIT) + '.pdf')
+    RECONSTRUCTION_MORPHED_OUTPUT_PLOT_PDF = PdfPages(OUT_DIREC + 'MORPHED' + str(File))
+    
+    
+    # creates directory for output files
+    OUT_DIREC = os.getcwd() + "/OUTPUT_FILES_PDF/"
+    '''
+with PdfPages(output_og_pdf) as pdf:     
+    for spectra_index in range(STARTS_FROM, ENDS_AT + 1): 
+        #keeping track of how many spectra do not have data points within the velocity limits
+        norm_spectrum_file_name = norm_spectra_list[spectra_index - 1]
+        File = (os.getcwd() + "/Recons_Hiremath2025/" + str(norm_spectrum_file_name))
         
-
+        #draw_abs_figure(spectra_count_abs, spectra_index, velocity, flux_normalized, error, savefile_name, spectra_name, redshift, snr, max_peak):
+        fig = Plot_spec_compare_og(File)
         
-        
-        
+        pdf.savefig(fig)
+        plt.show()
+        plt.close(fig)
