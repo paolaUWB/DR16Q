a39bba2fbd980e07bd1312d8a0c09f03f07472c1
Elijah Facklam <boyzombie04@gmail.com>
2026-02-17 18:23:44 -0800

    Updated Figure loading
    changed it so in the loop it closes the global storage of figures

--- ABSORPTION/XRAY/x-ray_recon_comparison.py
+++ ABSORPTION/XRAY/x-ray_recon_comparison.py
@@ -47,54 +47,37 @@ output_og_pdf = "spectra_output_og.pdf"
 
 with PdfPages(output_morphed_pdf) as pdf:
     for spectra_index in range(STARTS_FROM, ENDS_AT + 1):
+        #closes the global storage of figures
+        plt.close('all')
 
+        plt.figure()
+        #keeping track of how many spectra do not have data points within the velocity limits
         norm_spectrum_file_name = norm_spectra_list[spectra_index - 1]
         File = os.getcwd() + "/Recons_Hiremath2025/" + str(norm_spectrum_file_name)
 
         fig = Plot_spec_compare_morphed(File)
-
+        print("check " + str(spectra_index) + ', ' + str(norm_spectrum_file_name))
         pdf.savefig(fig)
-        plt.show()
+        plt.show(fig)
         plt.close(fig)
 
-#for spectra_index in range(STARTS_FROM, ENDS_AT + 1): 
-#    #keeping track of how many spectra do not have data points within the velocity limits
-#    
-#    norm_spectrum_file_name = norm_spectra_list[spectra_index - 1]
-#    File = (os.getcwd() + "/Recons_Hiremath2025/" + str(norm_spectrum_file_name))
-#    fig = Plot_spec_compare_morphed(File)
-#    PdfPages.savefig(fig)
-#    fig.save()
-#    pyplot.gcf()
-#    plt.show()
-#    plt.close()
     
     '''
-    data = File[1].data   
-    
-    
-    we want to compare, original, reconstruction and error in terms of velocity (check line 75)
+    we want to compare, original, reconstruction and error in terms of velocity
     look at spectra comparison program, examples of what the flux needs to be
     
     can feed the reconstruction and the original through the function draw_abs_figure; inside abs_plot_module
     alternatively, call the function defined within the spectra_comparison that can make the plots displayed, fairly automated
     
-    whichever one was chosen, have it produce into a pdf, we found a function in absorption.py; check line 13
-    
+    whichever one was chosen, have it produce into a pdf, we found a function in absorption.py; check line 14
     '''
-    #draw_abs_figure(spectra_count_abs, spectra_index, velocity, flux_normalized, error, savefile_name, spectra_name, redshift, snr, max_peak):
-    
-    
-    '''
-    RECONSTRUCTION_MORPHED_OUTPUT_PLOT_PDF = PdfPages(OUT_DIREC + 'BI' + str(BALNICITY_INDEX_LIMIT) + '.pdf')
-    RECONSTRUCTION_MORPHED_OUTPUT_PLOT_PDF = PdfPages(OUT_DIREC + 'MORPHED' + str(File))
-    
-    
-    # creates directory for output files
-    OUT_DIREC = os.getcwd() + "/OUTPUT_FILES_PDF/"
-    '''
+
+
 with PdfPages(output_og_pdf) as pdf:     
     for spectra_index in range(STARTS_FROM, ENDS_AT + 1): 
+        #closes the global storage of figures
+        plt.close('all')
+        plt.figure()
         #keeping track of how many spectra do not have data points within the velocity limits
         norm_spectrum_file_name = norm_spectra_list[spectra_index - 1]
         File = (os.getcwd() + "/Recons_Hiremath2025/" + str(norm_spectrum_file_name))
@@ -103,5 +86,5 @@ with PdfPages(output_og_pdf) as pdf:
         fig = Plot_spec_compare_og(File)
         
         pdf.savefig(fig)
-        plt.show()
+        plt.show(fig)
         plt.close(fig)
