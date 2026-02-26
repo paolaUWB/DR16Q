d6ca221ce02fb8f77a8f312ec777fce2603d9b52
Elijah Facklam <boyzombie04@gmail.com>
2026-02-25 19:45:57 -0800

    Combined functions to have parameters
    As title states as well as cleaned up and removed unnecessary code added titles for each section. made a reusable function for getting pdfs out called Spectra_Comparison_Generate_PDF
    
    Added my name to the file

--- ABSORPTION/XRAY/x-ray_recon_comparison.py
+++ ABSORPTION/XRAY/x-ray_recon_comparison.py
@@ -3,7 +3,7 @@
 """
 Created on Fri Feb  6 15:25:15 2026
 
-@author: lilianaflores
+@author: lilianaflores and elijahfacklam
 """
 
 import os
@@ -15,30 +15,18 @@ sys.path.insert(0, os.getcwd()+'/../')
 from utility_functions import read_list_spectra
 sys.path.insert(0, os.getcwd()+'/../')
 
-"""
-Great work on the x-ray_recon_comparison.py program! Some ideas for improvements to the program and the plotting functions called:
-The function already allows for providing xlimits. We should run with x limits -70,000 to 0 in order to see the range we are looking for EHVOs in.
-Automatic y limits: Could be by having the function find the max y value of the spectra within the x limits.
-Clean any excess unused code or comments
-Makes things more compact (optional): Combine the functions for og vs recon and morphed vs recon. Could be with if statements and a function parameter.
-"""
+
+######################################## PATH FINDING ########################################
+
 #defining the config file
 CONFIG_FILE = sys.argv[1] if len(sys.argv) > 1 else os.getcwd()+"/spec_lists/xray_list_SNR10_z1.9.csv" #250 with SNR>10 & z>1.9
 
 # range of spectra you are working with from the good_fit.csv file
-STARTS_FROM, ENDS_AT = 1, 250 # eventually 1 -> 250
+STARTS_FROM, ENDS_AT = 1, 25 # eventually 1 -> 250
 
 norm_spectra_list, redshift_list, calc_snr_list = read_list_spectra(CONFIG_FILE, ["NORM SPECTRA FILE NAME", "REDSHIFT", "CALCULATED SNR"]) 
 
-
-vlast = []
-
-# whether abs_count or all_count is used is based on the value of all_plot_and_text|
-abs_count = 0 # counter for amount of spectra that have absorption when all_plot_and_text = no and for text files
-all_count = 0 # counter for all spectra ran when all_plot_and_text = yes
-
-#keeping track of how many spectra do not have data points within the velocity limits
-not_in_range = []
+########################################## VARIABLES ##########################################
 
 # loops over each spectra from a specified starting and ending point
 output_folder = os.path.join(os.getcwd(), "OUTPUT_FILES_PDF")
@@ -47,42 +35,39 @@ output_morphed_pdf = os.path.join(output_folder, "spectra_output_morphed.pdf")
 output_og_pdf = os.path.join(output_folder, "spectra_output_og.pdf")
 xlims = -70000, 0
 
-with PdfPages(output_morphed_pdf) as pdf:
-    for spectra_index in range(STARTS_FROM, ENDS_AT + 1):
-        #closes the global storage of figures
-        plt.close('all')
+# Modes can be "morphed", "og", or "both"
+MODE = "both"
 
-        plt.figure()
-        
-        #keeping track of how many spectra do not have data points within the velocity limits
-        norm_spectrum_file_name = norm_spectra_list[spectra_index - 1]
-        #gets the cwd and and names each files corectly
-        File = os.getcwd() + "/Recons_Hiremath2025/" + str(norm_spectrum_file_name)
+########################################## FUNCTIONS ##########################################
 
-        #Plot of x-ray selected quasar spectra comparing the Morphed, Normalized, and Reconstruction spectra. Found in spectra_comparison.py
-        fig = Plot_spec_compare_morphed(File, xlims)
-        
-        #Saves the current figure into a page of the pdf
-        pdf.savefig(fig)
-        plt.show(fig)
-        plt.close(fig)
+def Spectra_Comparison_Generate_PDF(output_path, plot_function, xlims):
+    
+    with PdfPages(output_path) as pdf:
+        for spectra_index in range(STARTS_FROM, ENDS_AT + 1):
+            #closes the global storage of figures
+            plt.close('all')
+    
+            plt.figure()
+            
+            #keeping track of how many spectra do not have data points within the velocity limits
+            norm_spectrum_file_name = norm_spectra_list[spectra_index - 1]
+            #gets the cwd and and names each files corectly
+            File = os.getcwd() + "/Recons_Hiremath2025/" + str(norm_spectrum_file_name)
+    
+            #Plot of x-ray selected quasar spectra comparing the Morphed, Normalized, and Reconstruction spectra. Found in spectra_comparison.py
+            fig = plot_function(File, xlims)
+            
+            #Saves the current figure into a page of the pdf
+            pdf.savefig(fig)
+            plt.show(fig)
+            plt.close(fig)
+            
+if MODE == "morphed":
+    Spectra_Comparison_Generate_PDF(output_morphed_pdf, Plot_spec_compare_morphed, xlims)
+    
+elif MODE == "og":
+    Spectra_Comparison_Generate_PDF(output_og_pdf, Plot_spec_compare_og, xlims)
 
-
-
-with PdfPages(output_og_pdf) as pdf:     
-    for spectra_index in range(STARTS_FROM, ENDS_AT + 1): 
-        #closes the global storage of figures
-        plt.close('all')
-        
-        plt.figure()
-        #keeping track of how many spectra do not have data points within the velocity limits
-        norm_spectrum_file_name = norm_spectra_list[spectra_index - 1]
-        File = (os.getcwd() + "/Recons_Hiremath2025/" + str(norm_spectrum_file_name))
-        
-        #Plot of x-ray selected quasar spectra comparing the original spectrum and reconstruction. Found in spectra_comparison.py
-        fig = Plot_spec_compare_og(File, xlims)
-        
-        #creates a fig and uses the fucntion found in spectra_comparison.py
-        pdf.savefig(fig)
-        plt.show(fig)
-        plt.close(fig)
+elif MODE == "both":
+    Spectra_Comparison_Generate_PDF(output_morphed_pdf, Plot_spec_compare_morphed, xlims)
+    Spectra_Comparison_Generate_PDF(output_og_pdf, Plot_spec_compare_og, xlims)
